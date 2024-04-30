13f35571fb54d899ded3d6781009d0c1a973bc8c
Aleksandr Kuzmin <grinova@gmail.com>
2024-04-30 21:46:44 +0300

    Search by channels

--- src/components/channels/channels.tsx
+++ src/components/channels/channels.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { M3uPlaylist, M3uMedia } from 'm3u-parser-generator'
 import { Grid, GridCellProps } from 'react-virtualized'
 import cn from 'classnames'
@@ -6,6 +6,7 @@ import cn from 'classnames'
 import { useResizeObserver } from '$/hooks/use-resize-observer'
 
 import { Channel } from '../channel/channel'
+import { TextBox } from '../textbox/textbox'
 
 import './channels.scss'
 
@@ -26,20 +27,28 @@ export const Channels: React.FC<ChannelsProps> = (props) => {
 
   const { ref: targetRef, width, height } = useResizeObserver<HTMLDivElement>()
 
+  const [query, setQuery] = useState<string>('')
+
+  const columnsCount = Math.floor((width - SCROLL_WIDTH) / (COLUMN_WIDTH + COLUMNS_GAP_MIN))
+  const columnWidth = Math.floor((width - SCROLL_WIDTH) / columnsCount)
+
+  const medias = useMemo(() => {
+    if (query == '') {
+      return playlist.medias
+    }
+    return playlist.medias.filter((m) => m.name?.includes(query))
+  }, [playlist, query])
+
   const handleClick = (media: M3uMedia): void => {
     onChange?.(media);
   }
 
-  const columnsCount = Math.floor((width - SCROLL_WIDTH) / (COLUMN_WIDTH + COLUMNS_GAP_MIN))
-
-  const columnWidth = (width - SCROLL_WIDTH) / columnsCount
-
   const cellRenderer = (props: GridCellProps) => {
     const index = props.rowIndex * columnsCount + props.columnIndex
-    if (index >= playlist.medias.length) {
+    if (index >= medias.length) {
       return null;
     }
-    const media = playlist.medias[index]
+    const media = medias[index]
     const horizontalMargin = Math.floor((columnWidth - COLUMN_WIDTH) / 2)
     const style = {
       ...props.style,
@@ -61,18 +70,21 @@ export const Channels: React.FC<ChannelsProps> = (props) => {
 
   return (
     <div className={cn('channels', className)}>
-      <div ref={targetRef} className='channels__grid-container'>
-        {columnsCount > 0 &&
-          <Grid
-            width={width}
-            height={height}
-            columnWidth={columnWidth}
-            rowHeight={ROW_HEIGHT}
-            columnCount={columnsCount}
-            rowCount={playlist.medias.length / columnsCount}
-            cellRenderer={cellRenderer}
-          />
-        }
+      <TextBox value={query} placeholder='Enter channel name' onChange={setQuery}/>
+      <div className='channels__grid-container'>
+        <div ref={targetRef} className='channels__grid-resizer'>
+          {columnsCount > 0 &&
+            <Grid
+              width={width}
+              height={height}
+              columnWidth={columnWidth}
+              rowHeight={ROW_HEIGHT}
+              columnCount={columnsCount}
+              rowCount={Math.ceil(medias.length / columnsCount)}
+              cellRenderer={cellRenderer}
+            />
+          }
+        </div>
       </div>
     </div>
   )
